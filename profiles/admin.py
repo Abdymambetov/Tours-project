cebd74606fcbb046943ebff74505de656694504a
Abdymambetov <meder0555@icloud.com>
2024-01-20 20:57:21 +0600

    initial commit

---
+++ profiles/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import OrderTour
+
+admin.site.register(OrderTour)
+
---
+++ tour/constants.py
@@ -0,0 +1,19 @@
+class TourConstants:
+    RATING_CHOICES = (
+        (1, 1),
+        (2,2),
+        (3,3),
+        (4,4),
+        (5,5)
+    )
+    DURATION_CHOICES = (
+        ("1", "День"), 
+        ("3", "Дня"), 
+        ("7", "Дней")
+    )
+    COMPLEXITY_CHOICES = (
+        ("Easy", "Легкий"),
+        ("Medium", "Средний"),
+        ("Hard", "Тяжелый"),
+        ("Extra", "Экстра"),
+    )
